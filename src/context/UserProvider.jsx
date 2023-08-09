58e16311c4b9bf33b44d38ff3afa4c09ff0247c6
Luis Ángel Herrera <luis_mh@outlook.es>
2023-08-09 13:24:06 -0600

    establecer usuario

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react"
 import { UserContext } from "./userContext"
 
 
-const user = {
-    id:123,
-    name:"Luis Mondragon",
-    email:"luis_mh@outlook.es"
-}
+// const user = {
+//     id:123,
+//     name:"Luis Mondragon",
+//     email:"luis_mh@outlook.es"
+// }
 
 export const UserProvider = ({ children }) => {
+
+    const [user, setUser] = useState();
+
   return (
     
-    <UserContext.Provider value={{ hola: 'Mundo', user }}>
+    <UserContext.Provider value={{ user, setUser }}>
         { children }
     </UserContext.Provider>
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,32 @@
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
 
 
 export const LoginPage = () => {
+
+  const { user, setUser } = useContext( UserContext );
+
   return (
     <>
         <h1>LoginPage</h1>
         <hr />
+
+        <pre>
+
+            {
+              JSON.stringify(user, null, 3)
+            }
+
+        </pre>
+
+        <button className="btn btn-primary" onClick={ () => setUser({
+          id:123,
+          name:"Luis Mondragon",
+          email:"luis_mh@outlook.es"
+        }) }>
+          Establecer usuario
+        </button>
+
     </>
   )
 }
